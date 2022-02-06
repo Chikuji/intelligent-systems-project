b0ad2fb3897ee1402c645326af57c2b000b797a2
Chikuji <fkc07@live.com>
2022-02-06 18:00:41 -0300

    Servidor Flask - api.py e input.json básico

--- server/api.py
+++ server/api.py
@@ -1,9 +1,35 @@
 from flask import Flask
+from flask import request
+import os
+import pickle
+
+
+# path variables
+
+MODEL_PATH    = os.environ['MODEL_PATH']
+
+# Carregando a Máquina Preditiva
+pickle_in = open(MODEL_PATH, 'rb')
+
+ml_model = pickle.load(pickle_in)
+
 
 api = Flask(__name__)
 
-@api.route("/v1/categorize", methods=["GET"])
-def hello():
-    return { "messagem" : "Hello, Felipe"}
+@api.route("/hello/<name>", methods=["GET"])
+def hello(name):
+    return { "messagem" : f"Hello, {name}"}
 
+
+@api.route("/bye", methods=["POST"])
+def bye():
+    title = request.json["title"],
+    query = request.json["query"],
+    concatenated_tags = request.json["concatenated_tags"]
+
+    concat = str(title) + " " + str(query) + " " + str(concatenated_tags)
+
+    resut = ml_model.predict([concat])
+
+    return { "categories" : f"{resut}"}
 
